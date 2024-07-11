ed8dd1b07f1a7599632f2abf3f52de5c3318d9cc
Alina Vagner <ali.vag977@gmail.com>
2024-07-11 17:19:44 +0300

    add date to top

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,17 +2,25 @@ import React from "react";
 import { Participants } from "@components/Participants/Participants";
 import { IconButton } from "@components/common/IconButton/IconButton";
 import "./Header.scss";
+import { getDate } from "utils/utils";
 
 export const Header = () => {
   return (
-    <div className="header">
-      <Participants />
-      <div className="header__naming header-part">
-        <h2>🦄 Team Unicorns</h2>
-        <span>last seen 45 minutes ago</span>
+    <div className="relative">
+      <div className="header">
+        <Participants />
+        <div className="header__naming header-part">
+          <h2>🦄 Team Unicorns</h2>
+          <span>last seen 45 minutes ago</span>
+        </div>
+        <div className="header__panel header-part">
+          <IconButton name="dots-horizontal-rounded" />
+        </div>
       </div>
-      <div className="header__panel header-part">
-        <IconButton name="dots-horizontal-rounded" />
+      <div className="date-container">
+        <div className="date">
+          <span>{getDate(new Date().toISOString())}</span>
+        </div>
       </div>
     </div>
   );
