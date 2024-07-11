9ad71d7aaa107eea648a81f5fb4bb6fb4dfbb78f
Alina Vagner <ali.vag977@gmail.com>
2024-07-11 20:14:21 +0300

    try fix IOS input

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,12 @@ import "@styles/globals.scss";
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </head>
       <body>
         <ConfigProvider
           theme={{
