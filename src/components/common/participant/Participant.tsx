6b497a313018cb61a344c2c5748d171bf05403e4
Alina Vagner <ali.vag977@gmail.com>
2024-07-23 13:45:51 +0300

    use antd

--- src/components/common/participant/Participant.tsx
+++ src/components/common/participant/Participant.tsx
@@ -1,26 +1,18 @@
-"use client";
-
-import React, { SyntheticEvent, useState } from "react";
-import Image from "next/image";
+import React from "react";
+import { Image } from "antd";
 import "./Participant.scss";
 
 const anonimusUser = "./users/anonimus.jpg";
 
 export const Participant = ({ src, style, isOnline }: Props) => {
-  const [imgSrc, setImgSrc] = useState(src ?? anonimusUser);
-
   return (
     <div className={`relative h-8 ${style}`}>
       <div className={`participant`}>
         <Image
-          unoptimized
-          src={imgSrc}
-          loader={() => imgSrc}
+          src={src}
+          fallback={anonimusUser}
           alt="user"
-          width={500}
-          height={500}
           className="w-full h-full"
-          onError={() => setImgSrc(anonimusUser)}
         />
       </div>
       {isOnline && <div className="online" />}
