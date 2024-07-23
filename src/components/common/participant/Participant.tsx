943930da94a5d3b27f1df2b53f395277c6e47eb3
Alina Vagner <ali.vag977@gmail.com>
2024-07-23 11:16:13 +0300

    fix users for incognito

--- src/components/common/participant/Participant.tsx
+++ src/components/common/participant/Participant.tsx
@@ -1,15 +1,27 @@
-import React from "react";
-import { Image } from "antd";
+"use client";
 
+import React, { SyntheticEvent, useState } from "react";
+import Image from "next/image";
 import "./Participant.scss";
 
 const anonimusUser = "./users/anonimus.jpg";
 
 export const Participant = ({ src, style, isOnline }: Props) => {
+  const [imgSrc, setImgSrc] = useState(src ?? anonimusUser);
+
   return (
     <div className={`relative h-8 ${style}`}>
       <div className={`participant`}>
-        <Image src={src ?? anonimusUser} alt="user" className="w-full h-full" />
+        <Image
+          unoptimized
+          src={imgSrc}
+          loader={() => imgSrc}
+          alt="user"
+          width={500}
+          height={500}
+          className="w-full h-full"
+          onError={() => setImgSrc(anonimusUser)}
+        />
       </div>
       {isOnline && <div className="online" />}
     </div>
